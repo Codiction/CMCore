73f81859df91cdac15725815f683d179ee826d5e
Codiction <brent.c@codiction.com>
2013-07-27 23:03:38 +0200

    Initial Commit

---
+++ src/com/codiction/Main.java
@@ -0,0 +1,21 @@
+/*
+ * ==== CMCore License V1.0 ====
+ * 
+ * This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+ * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/ or send a letter to
+ * Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
+ * 
+ * If you do not agree to the license, please do not use this software.
+ * Removing this license text is NOT allowed.
+ */
+package com.codiction;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author Codiction
+ */
+public class Main extends JavaPlugin {
+    
+}
